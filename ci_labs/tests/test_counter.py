597f9f317189c34d3b2ea06692509f6c67fb0148
unknown <jparmstrong4@gmail.com>
2026-02-16 22:02:46 -0800

    fixed and finished CI notification and completed gettotal counter test

--- ci_labs/tests/test_counter.py
+++ ci_labs/tests/test_counter.py
@@ -110,6 +110,8 @@ class TestCounterEndpoints:
         assert response.status_code == HTTPStatus.OK
         
         # TODO: Add an assertion to check the correct total value
+        data = response.get_json()
+        assert data['total'] == 1
 
     # ===========================
     # Test: Retrieve top N highest counters
